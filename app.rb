7987b4eea8242573740b9160376d85b9cc783521
Romeo Gono <romeogono1@aol.com>
2018-12-04 00:53:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, 'secret'
+  end
+  
+  get '/' do
+    @session = session
+    
+    erb :index
+  end
+  
+  get '/hey' do
+    session["name"] = "Victoria"
+  @session = session
+end 
+
 end
